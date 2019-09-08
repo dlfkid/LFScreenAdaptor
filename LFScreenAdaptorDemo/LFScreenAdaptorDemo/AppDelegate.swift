2c4361c046a01946269fb38e8ba76639d7ededde
LingFeng <dlfkid@icloud.com>
2019-09-08 16:18:52 +0800

    更新文档

--- LFScreenAdaptorDemo/LFScreenAdaptorDemo/AppDelegate.swift
+++ LFScreenAdaptorDemo/LFScreenAdaptorDemo/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        LFScreenAdaptor.sharedAdaptor.designScreen = .Type5_8
         return true
     }
 
